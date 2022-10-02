3e24f74b94ac4bcf15c34c293b5e53828eb3ba0b
豆浆白倒 <47612561+Mozai233@users.noreply.github.com>
2022-10-02 18:33:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git openwrtpackages https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+echo 'src-git openwrtpackages https://github.com/Mozai233/openwrtpackages' >>feeds.conf.default
 #echo 'src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
